d6ecb40a9a6c416acbb118f4ca62c0c08fc23334
Rusty Gunao <gunaorusty@gmail.com>
2022-10-17 18:51:16 +0800

    Add

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
 const write = require('./write')
 const read = require('./read')
 
-const cmd = process.argv[2]
+const cmd = process.argv
 
-write(cmd)
+// write(cmd)
+// console.log(read())
 
-console.log(read())
+if(cmd[2] == 'read') {
+    
+    const present = require('./present')
+    present(read())
+}
+
+if(cmd[2] == 'add') {
+
+    let note = {
+        id: cmd[3],
+        name: cmd[4],
+        age: cmd[5]
+    }
+    let oldNote = read()
+    const add = require('./add')
+    add(note, oldNote)
+
+    const present = require('./present')
+    present(read())
+}
--- write.js
+++ write.js
@@ -1,6 +1,6 @@
 const fs = require('fs')
 
-const write = function(data = "default") {
+const write = function(data=[]) {
     fs.writeFileSync('note.txt', data)
 }
 module.exports = write
